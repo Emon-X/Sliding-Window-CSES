a94188d7133c68c7f12f87229bca102c3eea6e17
EmonX <mdemonhossaince23013@gmail.com>
2025-09-15 22:53:16 +0600

    Update Solution

---
+++ Sliding Window Mex.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define pp pop_back()
+#define mod 1000000007
+#define endl "\n"
+#define N 200005
+ll dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+ll dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void domain_expension()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> s(n);
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    set<ll> mex_candidates;
+
+    map<ll,ll> freq;
+    for (int i = 0; i < n + k + 2; ++i)
+        mex_candidates.insert(i);
+
+    for (int i = 0; i < k; ++i)
+    {
+        freq[s[i]]++;
+        mex_candidates.erase(s[i]);
+    }
+    cout << *mex_candidates.begin() << " ";
+    for (int i = k; i < n; ++i)
+    {
+        freq[s[i - k]]--;
+        if (freq[s[i - k]] == 0)
+            mex_candidates.insert(s[i - k]);
+
+        freq[s[i]]++;
+        mex_candidates.erase(s[i]);
+        cout << *mex_candidates.begin() << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    domain_expension();
+    return 0;
+}
